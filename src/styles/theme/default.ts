0763d6ceaea4e58bd8a1b47b30730e504fbdca90
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-07-04 22:16:59 -0300

    initial commit

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,18 @@
+export const defaultTheme = {
+  "base-white": "#ffffff",
+  "base-background": "#faf9f9",
+  "base-card": "#f2f1f1",
+  "base-input": "#edecec",
+  "base-button": "#e6e5e5",
+  "base-hover": "#d7d5d5",
+  "base-label": "#8c8585",
+  "base-text": "#564f4c",
+  "base-subtitle": "#3f3937",
+  "base-title": "#262220",
+  "brand-purple-dark": "#4b2894",
+  "brand-purple": "#7f46f7",
+  "brand-purple-light": "#ebe5f9",
+  "brand-yellow-dark": "#c47e16",
+  "brand-yellow": "#dbac2b",
+  "brand-yellow-light": "#f0e8c9",
+};
